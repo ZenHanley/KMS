b2b0241ec6898d38faecb182a25f925c36bc2f37
Zen <zenhanleyreisser@gmail.com>
2025-06-15 21:06:41 +0200

    Push 5

--- _data/client.js
+++ _data/client.js
@@ -1,20 +1,17 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "KMS Signs",
+    "phoneForTel": "413 664 8059",
+    "phoneFormatted": "(413) 664-8059",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "343 State St",
+        "city": "North Adams",
+        "state": "MA",
+        "zip": "01247",
         "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "mapLink": "https://www.google.com/maps/place/KMS+Signs/@42.6901346,-73.1156092,17z/data=!3m1!4b1!4m6!3m5!1s0x89e0b8225e1f3ead:0x74d220332ce45d78!8m2!3d42.6901307!4d-73.1130343!16s%2Fg%2F11ckqmqq_n?entry=ttu&g_ep=EgoyMDI1MDYxMS4wIKXMDSoASAFQAw%3D%3D"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/p/K-M-S-Signs-100066418815101/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://www.example.com"
